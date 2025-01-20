902652351ead856f6a2d9557f8c2f1743135368f
MaryiaKavalchuk <maryiakavalchuk97@gmail.com>
2025-01-20 21:32:01 +0300

    lesson7-2

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -11,7 +11,7 @@
 */
 
 function truncate(str, maxLength) {
-  return (str.length > maxLength) ? str.slice (0, maxLength - 1) + "..." : str
+  return (str.length > maxLength) ? str.slice (0, maxLength - 3) + "..." : str
 }
 console.log(truncate("Вот, что мне действительно нравится в этом", 20)); // Ожидаемый результат: "Вот, что мне де..."
 console.log(truncate("Короткая строка", 20)); // Ожидаемый результат: "Короткая строка"
